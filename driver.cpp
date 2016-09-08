5bed41d546c85192d2ad177a01c5b72176d942cf
MaveyMa <maveyma3891@student.hartnell.edu>
2016-09-08 14:31:33 -0700

    Ready to begin..

--- driver.cpp
+++ driver.cpp
@@ -3,6 +3,7 @@
 //
 // Summary: CSS3 Lab 4 | Floating-Point Numbers
 // Objective: Emulate how floating point values are stored in memory
+// Instructions: Implement a floating point number system. You can use the "simple model"
 //
 // Author: Mavey Ma
 // Due: September 10, 2016
@@ -16,7 +17,7 @@ using namespace std;
 
 int main()
 {
-    cout << "HELLO  GRAVY  GIRL!" << endl;
+    cout << "HELLO  GRAVY  GIRL!!" << endl;
 
     return EXIT_SUCCESS;
 }
